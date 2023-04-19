12a04fd2a9fd8dc3a42ab4ef6ec7b4a1e9184c6d
Josh Fraley <fraley.j3@gmail.com>
2023-04-19 10:48:57 -0500

    Capstone initial push

---
+++ BDGG/index.js
@@ -0,0 +1,5 @@
+function openForm() {
+  document.getElementById("burger").style.display = "inline";
+  document.getElementById("nav").style.display = "block";
+  document.getElementById("main").style.display = "none";
+  };
