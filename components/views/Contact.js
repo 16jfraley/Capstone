f35fd288f23f9d2b24d9c598e20eaeb493b6e5fb
Josh Fraley <fraley.j3@gmail.com>
2023-05-03 09:44:16 -0500

    file name

--- components/views/Contact.js
+++ components/views/Contact.js
@@ -1,7 +1,7 @@
 import html from "html-literal";
 import clinton from "../../assests/images/general/Clinton.jpg"
-import facebook from "../../assests/images/facebook.jpg"
-import gram from "../../assests/images/Instagram.jpg"
+import facebook from "../../assests/images/icons/facebook.jpeg"
+import gram from "../../assests/images/icons/instagram.jpeg"
 
 export default () => html`
 <main id="main">
