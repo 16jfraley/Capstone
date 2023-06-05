4ad3a0a08b142867b9852a5dc45e23cbfa823780
Josh Fraley <fraley.j3@gmail.com>
2023-06-05 18:30:33 -0500

    mission staitment

--- components/views/Home.js
+++ components/views/Home.js
@@ -14,11 +14,13 @@ export default () => html`
  <img class="image" id='imgOne' src="${clinton_16}" />
  <div class="content">
     <img class="image" id='imgTwo' src="${clinton_16}" />
- <h1 class="mission">
-    Our Mission:
-    <br>
-    Is to to....
-    </h1>
+ <h3 class="mission">
+
+ The Bi-State Disc Golf Group promotes the growth of disc golf in the Illinois and Missouri Bi-State area through the funding and promotion
+ of educational clinics, the organization of disc golf events and/or tournaments, both sanctioned and un-sanctioned by the Professional Disc Golf Association
+ (PDGA), and the establishment of an active player base for the purpose of increasing disc golf activity in the area. Additionally, the Bi-State Disc Golf Group provides volunteer manpower and funding for
+ the establishment of new disc golf courses and the maintenance and improvement of current disc golf courses in the Illinois/Missouri area.
+    </h3>
 
 </div>
  </div>
